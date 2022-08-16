e048ad9b5fa9a6e60a8ed03ab63aa4fcb3f91be1
Melvin van Gent <melvinvangent@gmail.com>
2022-08-16 09:50:22 +0200

    Add to do

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -14,7 +15,7 @@ type model struct {
 
 func initialModel() model {
 	return model{
-		choices:  []string{"Make backend", "Read release branche version numbers", "Add prompt by JIRA issue"},
+		choices:  []string{"Make backend", "Read release branche version numbers", "Add prompt by JIRA issue", "Add spectacular ASCI message"},
 		selected: make(map[int]struct{}),
 	}
 }
@@ -23,7 +24,71 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func main() {
-	fmt.Println("Hello world!")
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
 
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		case "enter", " ", "x":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "What should vpofe still do for just in time - a has my release made it to production TUI - a simple check app to save time? \n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+
+	if err := p.Start(); err != nil {
+		fmt.Printf("Something went wrong, ups. Error: %v", err)
+		os.Exit(1)
+	}
 }

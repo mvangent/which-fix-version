b36c2215b32132b061cd1bdb6c862b2933b013c2
Melvin van Gent <melvinvangent@gmail.com>
2022-08-29 01:06:08 +0200

    Connect go-git command to text inputs interface

--- main.go
+++ main.go
@@ -1,155 +1,4 @@
 package main
-/*
-import (
-	"fmt"
-	// "strings"
-	//"net/http"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-	// c "github.com/vpofe/just-in-time/httpclient"
-	git "github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-git/go-git/v5/storage/memory"
-)
-
-type model struct {
-	commitHash   string
-	err          error
-	foundRelease string
-    ticket       string
-}
-
-var url = "https://github.com/vpofe/just-in-time"
-
-// CheckIfError should be used to naively panics if an error is not nil.
-func CheckIfError(err error) {
-	if err == nil {
-		return
-	}
-
-	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
-	os.Exit(1)
-}
-
-// Info should be used to describe the example commands that are about to run.
-func Info(format string, args ...interface{}) {
-	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
-}
-
-func checkServer() tea.Msg {
-
-	// check for release versions
-	// Clones the given repository in memory, creating the remote, the local
-	// branches and fetching the objects, exactly as:
-	Info(url)
-
-	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-		URL: url,
-        // ReferenceName: "main",
-        // SingleBranch: true,
-	})
-
-	CheckIfError(err)
-
-	// Gets the HEAD history from HEAD, just like this command:
-	Info("git log")
-
-	// ... retrieves the branch pointed by HEAD
-	ref, err := r.Head()
-	CheckIfError(err)
-
-	// ... retrieves the commit history
-	cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
-	CheckIfError(err)
-
-    // ... just iterates over the commits, printing it
-	err = cIter.ForEach(func(c *object.Commit) error {
-		fmt.Println(c)
-		return nil
-	})
-	CheckIfError(err)
-
-	if err != nil {
-		return errMsg(err)
-	}
-
-	// releases := strings.Split("--", string(stdout))
-
-	return statusMsg(string("great success"))
-
-	// return statusMsg(response.StatusCode)
-}
-
-type statusMsg string
-
-type errMsg error
-
-func (m model) Init() tea.Cmd {
-	return checkServer
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	case statusMsg:
-		// The server returned a status message. Save it to our model. Also
-		// tell the Bubble Tea runtime we want to exit because we have nothing
-		// else to do. We'll still be able to render a final view with our
-		// status message.
-		m.status = string(msg)
-		return m, tea.Quit
-
-	case errMsg:
-		// There was an error. Note it in the model. And tell the runtime
-		// we're done and want to quit.
-		m.err = msg
-		return m, tea.Quit
-
-     	case tea.KeyMsg:
-		// Ctrl+c exits. Even with short running programs it's good to have
-		// a quit key, just incase your logic is off. Users will be very
-		// annoyed if they can't exit.
-		if msg.Type == tea.KeyCtrlC {
-			return m, tea.Quit
-		}
-	}
-
-	// If we happen to get any other messages, don't do anything.
-	return m, nil
-}
-
-func (m model) View() string {
-	// If there's an error, print it out and don't do anything else.
-	if m.err != nil {
-		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
-	}
-
-	// Tell the user we're doing something.
-	s := fmt.Sprintf("Checking %s ... ", url)
-
-	// When the server responds with a status, add it to the current line.
-	if len(m.status) > 0 {
-		s += fmt.Sprintf("%d %s!", m.status)
-	}
-
-	// Send off whatever we came up with above for rendering.
-	return "\n" + s + "\n\n"
-}
-
-func main() {
-	p := tea.NewProgram(model{})
-
-	if err := p.Start(); err != nil {
-		fmt.Printf("Something went wrong, ups. Error: %v", err)
-		os.Exit(1)
-	}
-}
-
-package main
-*/
-// A simple example demonstrating the use of multiple text input components
-// from the Bubbles component library.
 
 import (
 	"fmt"
@@ -179,6 +28,8 @@ type model struct {
 	cursorMode textinput.CursorMode
 }
 
+var url = "https://github.com/vpofe/just-in-time"
+
 func initialModel() model {
 	m := model{
 		inputs: make([]textinput.Model, 3),
@@ -192,12 +43,13 @@ func initialModel() model {
 
 		switch i {
 		case 0:
-			t.Placeholder = "Nickname"
+			t.Placeholder = "Branch"
+			t.SetValue("master")
 			t.Focus()
 			t.PromptStyle = focusedStyle
 			t.TextStyle = focusedStyle
 		case 1:
-			t.Placeholder = "Email"
+			t.Placeholder = "Commit Hash"
 			t.CharLimit = 64
 		case 2:
 			t.Placeholder = "Password"
@@ -210,9 +62,13 @@ func initialModel() model {
 
 	return m
 }
+
 func (m model) Init() tea.Cmd {
 	return textinput.Blink
 }
+
+type statusMsg string
+type errMsg error
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
@@ -221,7 +77,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
 
-		// Change cursor mode
+			// Change cursor mode
 		case "ctrl+r":
 			m.cursorMode++
 			if m.cursorMode > textinput.CursorHide {
@@ -233,14 +89,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 			return m, tea.Batch(cmds...)
 
-		// Set focus to next input
+			// Set focus to next input
 		case "tab", "shift+tab", "enter", "up", "down":
 			s := msg.String()
 
 			// Did the user press enter while the submit button was focused?
 			// If so, exit.
 			if s == "enter" && m.focusIndex == len(m.inputs) {
-				return m, tea.Quit
+				return m, checkServer // tea.Quit
 			}
 
 			// Cycle indexes
@@ -323,3 +179,94 @@ func main() {
 	}
 }
 
+/*
+import (
+	"fmt"
+	// "strings"
+	//"net/http"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	// c "github.com/vpofe/just-in-time/httpclient"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/memory"
+)
+
+type model struct {
+	commitHash   string
+	err          error
+	foundRelease string
+    ticket       string
+}
+
+var url = "https://charm.sh"
+
+
+
+
+func (m model) Init() tea.Cmd {
+	return checkServer
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case statusMsg:
+		// The server returned a status message. Save it to our model. Also
+		// tell the Bubble Tea runtime we want to exit because we have nothing
+		// else to do. We'll still be able to render a final view with our
+		// status message.
+		m.status = string(msg)
+		return m, tea.Quit
+
+	case errMsg:
+		// There was an error. Note it in the model. And tell the runtime
+		// we're done and want to quit.
+		m.err = msg
+		return m, tea.Quit
+
+     	case tea.KeyMsg:
+		// Ctrl+c exits. Even with short running programs it's good to have
+		// a quit key, just incase your logic is off. Users will be very
+		// annoyed if they can't exit.
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
+	}
+
+	// If we happen to get any other messages, don't do anything.
+	return m, nil
+}
+
+func (m model) View() string {
+	// If there's an error, print it out and don't do anything else.
+	if m.err != nil {
+		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
+	}
+
+	// Tell the user we're doing something.
+	s := fmt.Sprintf("Checking %s ... ", url)
+
+	// When the server responds with a status, add it to the current line.
+	if len(m.status) > 0 {
+		s += fmt.Sprintf("%d %s!", m.status)
+	}
+
+	// Send off whatever we came up with above for rendering.
+	return "\n" + s + "\n\n"
+}
+
+func main() {
+	p := tea.NewProgram(model{})
+
+	if err := p.Start(); err != nil {
+		fmt.Printf("Something went wrong, ups. Error: %v", err)
+		os.Exit(1)
+	}
+}
+
+package main
+*/
+// A simple example demonstrating the use of multiple text input components
+// from the Bubbles component library.

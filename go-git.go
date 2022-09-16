ff513102e75c3b36c2ef7956ca335847536fb900
Melvin van Gent <melvinvangent@gmail.com>
2022-09-16 21:43:15 +0200

    List all remote refs and print them out

--- go-git.go
+++ go-git.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
 	git "github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
@@ -14,30 +15,27 @@ func checkServer() tea.Msg {
 	// check for release versions
 	// Clones the given repository in memory, creating the remote, the local
 	// branches and fetching the objects, exactly as:
-	Info("git clone https://github.com/go-git/go-billy")
+	Info("git clone the test repo")
 
-	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-		URL: "https://github.com/vpofe/just-in-time",
+	// Create the remote with repository URL
+	remote := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
+		Name: "origin",
+		URLs: []string{"https://github.com/vpofe/just-in-time"},
 	})
-
-	CheckIfError(err)
 
 	// Gets the HEAD history from HEAD, just like this command:
-	Info("git log")
+	Info("git list all remote branches")
 
-	// ... retrieves the branch pointed by HEAD
-	ref, err := r.Head()
-	CheckIfError(err)
+	refs, err := remote.List(&git.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// ... retrieves the commit history
-	cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
-	CheckIfError(err)
+	for _, ref := range refs {
+		fmt.Println(ref.String())
+	}
 
 	// ... just iterates over the commits, printing it
-	err = cIter.ForEach(func(c *object.Commit) error {
-		fmt.Println(c)
-		return nil
-	})
 	CheckIfError(err)
 
 	if err != nil {

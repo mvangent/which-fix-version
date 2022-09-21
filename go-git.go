214e5f8d0cb51eb3fec1acf7ab04b63f9b36b132
Melvin van Gent <melvinvangent@gmail.com>
2022-09-21 09:11:16 +0200

    Make release map and rootCandidates list

--- go-git.go
+++ go-git.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
 	"strings"
 	"time"
 
@@ -14,11 +15,15 @@ import (
 
 func (m model) findFixVersion() tea.Msg {
 
-	branches := getRemoteBranches()
+	rootCandidates, releases:= getRemoteBranches()
 
 	// fetch commit list from ma(in/ster)
-	root := getRoot(branches)
+	root := selectRoot(rootCandidates)
 	// check latest release
+
+    sortedReleases := getSortedReleases(releases)
+
+    fmt.Println(sortedReleases)
 	// if not present
 	// return not yet in any release
 	// if in release
@@ -30,24 +35,23 @@ func (m model) findFixVersion() tea.Msg {
 	return fixVersionMsg(root)
 }
 
-func getRoot(root map[string]string) string {
-	// TODO: this should come as default from a flag, lets have, main, master, development fallback
-	if _, exists := root["main"]; exists {
-		return "main"
-	} else {
-		if _, exists := root["master"]; exists {
-			return "master"
-		} else {
-			if _, exists := root["development"]; exists {
-				return "development"
-			} else {
-				panic("no root found")
-			}
-		}
-	}
+func getSortedReleases(releases map[string]string) []string {
+    versions := make([]string, 0)
+    for k := range releases {
+        versions = append(versions, k)
+    }
+
+    sort.Strings(versions)
+
+   return versions 
 }
 
-func getRemoteBranches() map[string]string {
+func selectRoot(rootCandidates []string) string {
+	// TODO: this should come as default from a flag, lets have, main, master, development fallback
+    return rootCandidates[0]
+}
+
+func getRemoteBranches() ([]string, map[string]string) {
 
 	Info("Get all remote branches")
 
@@ -65,35 +69,35 @@ func getRemoteBranches() map[string]string {
 		panic(err)
 	}
 
-	branches := map[string]string{}
+	releases := make(map[string]string)
+    rootCandidates := make([]string, 0)
 
 	for _, ref := range refs {
 		s := ref.String()
 		if strings.Contains(s, "refs/heads/") {
 			branchName := strings.SplitAfter(s, "refs/heads/")[1]
-
+            
 			var branchVersion string
 
 			if strings.Contains(branchName, "release/") {
-				branchVersion = strings.SplitAfter(branchName, "release/")[1]
+                branchVersion = strings.SplitAfter(branchName, "release/")[1]
+                releases[branchVersion] = branchName
 			} else if strings.Contains(branchName, "releases/") {
 				branchVersion = strings.SplitAfter(branchName, "releases/")[1]
+                releases[branchVersion] = branchName
 			} else if strings.Contains(branchName, "release-") {
 				branchVersion = strings.SplitAfter(branchName, "release-")[1]
+                releases[branchVersion] = branchName
 			} else if branchName == "main" || branchName == "master" || branchName == "development" {
-				branchVersion = "rootCandidate"
-			} else {
-				branchVersion = "source"
-			}
-
-			branches[branchName] = branchVersion
+                rootCandidates = append(rootCandidates, branchName)                
+            }	
 		}
 	}
 
 	// check if main or master
-	for _, branch := range branches {
-		fmt.Println(branch)
+	for k, v:= range releases {
+        fmt.Printf("releases: %s, branchName: %s \n", k, v)
 	}
 
-	return branches
+	return rootCandidates, releases
 }

512698ff0285b9741b16116157f0beb1d788025c
Melvin van Gent <melvinvangent@gmail.com>
2022-10-17 11:04:49 +0200

    Split find command from main file

--- cmd/wfv/main.go
+++ cmd/wfv/main.go
@@ -2,64 +2,9 @@ package main
 
 import (
 	"errors"
-	"flag"
 	"fmt"
 	"os"
-	"strings"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/vpofe/which-fix-version/app"
-	"github.com/vpofe/which-fix-version/git"
 )
-
-func NewFindCommand() *FindCommand {
-	fc := &FindCommand{
-		fs: flag.NewFlagSet("find", flag.ContinueOnError),
-	}
-
-	fc.fs.StringVar(&fc.commitHash, "commitHash", "", "the main/master/development/custom branch commit hash to find the minimal fix version")
-	fc.fs.StringVar(&fc.url, "url", "", "git repository url")
-	fc.fs.StringVar(&fc.remoteName, "remoteName", "origin", "remote name to fetch branches from")
-	fc.fs.StringVar(&fc.developmentBranchName, "developmentBranchName", "main", "name of the central development branch")
-	fc.fs.StringVar(&fc.releaseBranchPrependIdentifiers, "releaseBranchPrependIdentifiers", "release- releases/ release/", "all string characters before the release version")
-
-	return fc
-}
-
-type FindCommand struct {
-	fs *flag.FlagSet
-
-	url                             string
-	remoteName                      string
-	developmentBranchName           string
-	releaseBranchPrependIdentifiers string
-	commitHash                      string
-}
-
-func (g *FindCommand) Name() string {
-	return g.fs.Name()
-}
-
-func (g *FindCommand) Init(args []string) error {
-	return g.fs.Parse(args)
-}
-
-func (g *FindCommand) Run() error {
-	app := app.NewApp(&git.GitConfig{
-		CommitHash:                      g.commitHash,
-		URL:                             g.url,
-		RemoteName:                      g.remoteName,
-		DevelopBranchName:               g.developmentBranchName,
-		ReleaseBranchPrependIdentifiers: strings.Split(g.releaseBranchPrependIdentifiers, " "),
-	})
-
-	if err := tea.NewProgram(app.Model).Start(); err != nil {
-		fmt.Printf("could not start program: %s\n", err)
-		os.Exit(1)
-	}
-
-	return nil
-}
 
 type Runner interface {
 	Init([]string) error
